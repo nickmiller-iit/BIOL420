4df41368e669494d2489cc2d9d2336559d4e2822
nickmiller-unl <nmiller4@unl.edu>
2015-09-22 17:12:44 -0500

    Update to use our own simulation and tree drawing code

--- basic_coalescent/server.R
+++ basic_coalescent/server.R
@@ -6,20 +6,17 @@
 #
 
 library(shiny)
-library(coalesceR)
+source("./coalesce.R")
 
 shinyServer(function(input, output) {
 
   output$treePlot <- renderPlot({
     #use action buttin to force new
     input$next_sim
-    tree <- sim.tree(method = "generations",
-                     sample = input$lineages,
-                     current = input$N,
-                     ancestral = input$N,
-                     time = 1)
-    draw.tree(tree,
-              labels = F)
+    coal <- sim.coal(two.N = input$N * 2,
+                     k = input$lineages)
+    draw.tree(coal)
+
   })
 
 })
